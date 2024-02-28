c9625a3f6c0857a0964d90ad50f1ac1b2e9a8345
rohmerpauline <rohmer.pauline@outlook.com>
2024-02-28 22:18:33 +0100

    Implement back end server

---
+++ server/routes/api/posts.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongodb = require('mongodb');
+const router = express.Router();
+
+// Get Blog Post
+router.get('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  res.send(await posts.find({}).toArray());
+});
+
+// Add Blog Post
+router.post('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  const { title, content } = req.body;
+  await posts.insertOne({
+    title: title,
+    content: content,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+  res.status(201).send();
+});
+
+// Delete Blog Post
+router.delete('/:id', async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.deleteOne({ _id: new mongodb.ObjectId(req.params.id) });
+});
+
+async function loadPostsCollection() {
+  const client = await mongodb.MongoClient.connect(
+    'mongodb+srv://rohmerpauline3:TEQOKjqOc5zkZG9U@vueexpressblog.rzomrkj.mongodb.net/?retryWrites=true&w=majority&appName=vueexpressblog',
+    {
+      useNewUrlParser: true,
+    },
+  );
+
+  return client.db('vueexpress').collection('posts');
+}
+
+module.exports = router;
