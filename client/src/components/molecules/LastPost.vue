11d474b80643c3cbad3270bcf34749f628b82c26
rohmerpauline <rohmer.pauline@outlook.com>
2024-03-02 19:46:51 +0100

    Modify HomeView

---
+++ client/src/components/molecules/LastPost.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="last-post" v-if="post">
+    <h2 class="post-heading" @click="handleOnClick(post._id)">{{ post.title }}</h2>
+    <p class="post-subheading">{{ post.subheading }}</p>
+    <p class="post-content">{{ post.content }}</p>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+defineProps({
+  post: Object,
+});
+
+const router = useRouter();
+
+const handleOnClick = (id) => {
+  router.push(`article/${id}`);
+};
+</script>
+
+<style scoped>
+.last-post {
+  margin: 40px 0;
+}
+
+.post-heading {
+  font-size: 40px;
+  font-weight: 700;
+  margin-bottom: 10px;
+  cursor: pointer;
+}
+
+.post-heading:hover {
+  color: var(--theme-color);
+  transform: scale(1.01);
+}
+
+.post-subheading {
+  font-size: 18px;
+  margin-bottom: 20px;
+}
+
+.post-content {
+  text-align: justify;
+  line-height: 2;
+}
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,24 +1,54 @@
+<template>
+  <LastPost :post="posts[0]" />
+  <Heading title="Latest blog posts" />
+  <PostsList :posts="posts" />
+  <div class="see-all-link"><RouterLink to="/articles">See all article</RouterLink></div>
+</template>
+
 <script setup>
 import Heading from '@/components/atoms/Heading.vue';
-import BlogCard from '@/components/molecules/BlogCard.vue';
-</script>
+import LastPost from '@/components/molecules/LastPost.vue';
+import PostsList from '@/components/organisms/PostsList.vue';
+import { useBlogPostStore } from '@/stores/BlogPostStore';
+import { ref, onMounted } from 'vue';
+import { RouterLink } from 'vue-router';
 
-<template>
-  <Heading title="Latest blog posts" />
-  <div class="blog-card-list">
-    <BlogCard />
-    <BlogCard />
-    <BlogCard />
-    <BlogCard />
-    <BlogCard />
-    <BlogCard />
-  </div>
-</template>
+const blogPostStore = useBlogPostStore();
+const posts = ref([]);
+
+const fetchPostsByLimit = async () => {
+  await blogPostStore.getPostsByLimit(5);
+  posts.value = blogPostStore.posts;
+};
+
+onMounted(() => {
+  fetchPostsByLimit();
+});
+</script>
 
 <style scoped>
 .blog-card-list {
   display: flex;
   flex-direction: column;
   gap: 15px;
+}
+
+.see-all-link {
+  margin: 20px 0;
+  text-align: center;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+.see-all-link:hover {
+  transform: scale(1.02);
+}
+
+a:hover {
+  color: var(--theme-color);
+  font-weight: 600;
 }
 </style>
